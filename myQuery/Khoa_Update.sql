b7db42db741716328e97fe7a455639f6d737ca28
punzloser <71777326+punzloser@users.noreply.github.com>
2020-09-30 11:10:56 +0700

    .

---
+++ myQuery/Khoa_Update.sql
@@ -0,0 +1,20 @@
+﻿use ScheduleManagement
+go
+create proc Khoa_Update
+
+(
+	@MaKhoa nvarchar(20),
+	@TenKhoa nvarchar(50),
+	@DienThoai nvarchar(20),
+	@Email nvarchar(50)
+)
+as
+begin
+	update Khoa
+	set	TenKhoa = @TenKhoa,
+		DienThoai = @DienThoai,
+		Email = @Email
+	where MaKhoa = @MaKhoa
+
+	select ERROR_MESSAGE = N'Sửa Thành Công !'
+end
